4e9ca6e423903a354f45c995786bbe26c210b0f4
unknown <dipisha03@gmail.com>
2018-02-25 14:54:35 -0500

    jawsdb setup stuff

--- models/index.js
+++ models/index.js
@@ -14,8 +14,8 @@ var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
 
-if (config.use_env_variable) {
-    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (env === 'production') {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
 } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
